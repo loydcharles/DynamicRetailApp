f44610f9287a3e122b19da566eafbffd717fd0c5
Mallory Harding <brian.harding@gmail.com>
2017-11-01 19:24:17 -0400

    some form width adjustments

--- client/src/components/Quantity/Input1.js
+++ client/src/components/Quantity/Input1.js
@@ -1,6 +1,14 @@
 import React from "react";
 
+var divStyle = {
+  size: 4
+};
+
 export const Input1 = props =>
-  <div className="form-group">
-    <input className="form-control" {...props} />
+  <div class="col-2" className="form-group" style={divStyle}>
+    <input className="form-control" {...props} maxlength="2"/>
   </div>;
+
+
+
+//ReactDOM.render(<div style={divStyle}>Hello World!</div>, mountNode);
